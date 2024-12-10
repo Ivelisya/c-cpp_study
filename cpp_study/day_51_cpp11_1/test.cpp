c43c121bf459f1ecdbe750981619344e682b7328
smazyu <138857469+FluffyBloomX@users.noreply.github.com>
2024-12-10 22:42:11 +0800

    1

--- cpp_study/day_51_cpp11_1/test.cpp
+++ cpp_study/day_51_cpp11_1/test.cpp
@@ -80,6 +80,57 @@ int main()
     //范围for 当容器中对象比较大，或者这个对象要做深拷贝,如string
     //最好给&和const,可以减少拷贝提高效率
     //auto 不能作为形参 和 返回值
+
+    //容器支持范围for的原理:范围for会被编译器编译成迭代器,意味着支持迭代器就支持范围for
 }
 #endif
+
+//除了stl的容器可以使用范围for用法，数组也可以(原生指针可以认为是天然的迭代器) 比如 vector string 的迭代器就是原生指针。
+
+//final 和 override 关键字
+//final 修饰的类不能被继承 修饰的虚函数不能被重写
+//override 修饰的虚函数必须是重写基类的虚函数 作为重写之后的标记
+
+//final 修饰类 类就变成最终类 不能被继承
+//override 是子类重写函数，检查是否完成重写。不满足重写的条件，则会报错
+
+//智能指针(熟悉)
+
+//新容器
+//c++98容器
+//string/vector/list/map/set + stack/queue/priority_queue
+//c++11容器
+//array(定长数组)/forward_list(单链表)/unordered_map/unordered_set/multimap/multiset
+#include <array>
+//array(定长数组): 实际用的很少，缺点：定长+存储的空间一般在栈上，栈上的空间有限
+//forward_list 单链表 不能尾插，只能头插 不能尾删，只能头删
+//unordered_map/unordered_set:推荐使用，因为他们的效率高于map/set
+// int main()
+// {
+//     array<int,100> a;
+//     return 0;
+// }
+//右值引用
+
+//默认成员函数控制
+//1.显式缺省成员函数
+
+//要求a的对象不能拷贝和赋值(防拷贝)
+//c++98 只声明不实现，这样就没办法用(拷贝对象) 缺陷 别人可以在类外实现
+class A
+{
+public:
+    A() = default;//显式缺省构造函数 编译器显示指定生成这个东西
+    A(const int &a):_a(a)
+    {
+
+    }
+private:
+    int _a;
+};
+int main()
+{
+    A a1;//无法通过编译 缺少默认构造函数 因为拷贝构造也是默认构造函数
+    return 0;
+}
 
