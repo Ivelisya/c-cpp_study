fd54295910bc571ff58f8520027dbe55298b4a2c
smazyu <138857469+Ivelisya@users.noreply.github.com>
2025-01-08 21:42:22 +0800

    1

--- cpp_study/day_56_smart_pointer_1/test1.cpp
+++ cpp_study/day_56_smart_pointer_1/test1.cpp
@@ -33,16 +33,34 @@ void f1()
     SmartPtr<int> sp(p);
     cout << div() << endl;
 };
-
+//智能指针的坑
+// int main()
+// {
+//     try
+//     {
+//         f1();
+//     }
+//     catch (exception& e)
+//     {
+//         cout << e.what() << endl;
+//     }
+//     return 0;
+// }
 int main()
 {
-    try
-    {
-        f1();
-    }
-    catch (exception& e)
-    {
-        cout << e.what() << endl;
-    }
+    SmartPtr<int> sp1(new int);
+    SmartPtr<int> sp2(sp1);
+    //三种解决方案
+    //1.管理权转移
+    //2.禁止拷贝
+    //3.引用计数
+
+    //原因是没有定义拷贝构造函数，所以会调用默认的拷贝构造函数，导致两个指针指向同一个地址，当其中一个指针释放内存后，另一个指针就会指向一个无效的地址
     return 0;
+
+
+    //c++98  auto_ptr 管理权转移
+    //c++11  unique_ptr 禁止拷贝
+    //c++11  shared_ptr 引用计数 会有循环引用的问题
+    //weak_ptr 解决循环引用问题
 }
