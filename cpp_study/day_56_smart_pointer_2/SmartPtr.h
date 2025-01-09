d9bfe9c0ed13ede9ece99ee5a0ad97d12a9e6321
smazyu <138857469+Ivelisya@users.noreply.github.com>
2025-01-09 16:40:56 +0800

    1

---
+++ cpp_study/day_56_smart_pointer_2/SmartPtr.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+using namespace std;
+// 帮我们管理资源的释放 无论是函数正常结束还是异常结束 都能保证资源的释放
+
+// RAII 利用对象生命周期来控制程序资源
+// RAII和智能指针的关系
+// RAII是一种托管资源的方法，智能指针是RAII的一种实现
+template <class T>
+class SmartPtr
+{
+public:
+    SmartPtr(T* ptr) : _ptr(ptr) {}
+
+    ~SmartPtr()
+    {
+        if (_ptr)
+        {
+            cout << "delete _ptr" << endl;
+            delete _ptr;
+        }
+    }
+
+    T& operator*() { return *_ptr; }
+
+    T* operator->() { return _ptr; }
+
+private:
+    T* _ptr;
+};
